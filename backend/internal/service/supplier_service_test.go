96f885cf0c26e28d105f6024b3df0d6565407606
Widdy Arfiansyah <widdyarfiansyah00@gmail.com>
2023-06-23 22:39:44 +0700

    test: supplier service functions have been implemented unit tests

---
+++ backend/internal/service/supplier_service_test.go
@@ -0,0 +1,329 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"inventory-management/backend/internal/http/presenter/request"
+	"inventory-management/backend/internal/http/presenter/response"
+	"inventory-management/backend/internal/model"
+	repository "inventory-management/backend/internal/repository/mock"
+	"testing"
+)
+
+func TestSupplierService_FindAll(t *testing.T) {
+	testCases := []struct {
+		name                             string
+		expectedSupplierRepoFindAll      []*model.Supplier
+		expectedSupplierRepoFindAllError error
+		expectedSvc                      []*response.SupplierResponse
+		expectedSvcError                 error
+	}{
+		{
+			name: "Number of suppliers more than 1",
+			expectedSupplierRepoFindAll: []*model.Supplier{
+				{
+					ID:      1,
+					Code:    "WDWDARFSYH",
+					Name:    "Widdy Arfiansyah",
+					Address: "Sukabumi",
+					Phone:   "082291832488",
+				},
+				{
+					ID:      2,
+					Code:    "HYSFRADWDW",
+					Name:    "Arfiansyah",
+					Address: "Cisaat",
+					Phone:   "089922234214",
+				},
+			},
+			expectedSvc: []*response.SupplierResponse{
+				{
+					ID:        1,
+					Code:      "WDWDARFSYH",
+					Name:      "Widdy Arfiansyah",
+					Address:   "Sukabumi",
+					Phone:     "082291832488",
+					CreatedAt: "0001-01-01 00:00:00 +0000 UTC",
+					UpdatedAt: "0001-01-01 00:00:00 +0000 UTC",
+				},
+				{
+					ID:        2,
+					Code:      "HYSFRADWDW",
+					Name:      "Arfiansyah",
+					Address:   "Cisaat",
+					Phone:     "089922234214",
+					CreatedAt: "0001-01-01 00:00:00 +0000 UTC",
+					UpdatedAt: "0001-01-01 00:00:00 +0000 UTC",
+				},
+			},
+			expectedSupplierRepoFindAllError: nil,
+			expectedSvcError:                 nil,
+		},
+		{
+			name:                             "Number of suppliers is 0 or null",
+			expectedSupplierRepoFindAll:      nil,
+			expectedSvc:                      nil,
+			expectedSupplierRepoFindAllError: nil,
+			expectedSvcError:                 nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			var repo repository.SupplierRepositoryMock
+			repo.On("FindAll", ctx).Return(tc.expectedSupplierRepoFindAll, tc.expectedSupplierRepoFindAllError)
+			svc := NewSupplierService(&repo)
+			result, err := svc.FindAll(ctx)
+			if tc.expectedSvcError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expectedSvcError.Error(), err.Error())
+			}
+
+			assert.Equal(t, tc.expectedSvc, result)
+			assert.Equal(t, len(tc.expectedSvc), len(result))
+		})
+	}
+}
+
+func TestSupplierService_FindByCode(t *testing.T) {
+	testCases := []struct {
+		name                                string
+		request                             string
+		expectedSupplierRepoFindByCode      *model.Supplier
+		expectedSupplierRepoFindByCodeError error
+		expectedSvc                         *response.SupplierResponse
+		expectedSvcError                    error
+	}{
+		{
+			name:    "Supplier exists with given code",
+			request: "WDWDARFSYH",
+			expectedSupplierRepoFindByCode: &model.Supplier{
+				ID:      1,
+				Code:    "WDWDARFSYH",
+				Name:    "Widdy Arfiansyah",
+				Address: "Sukabumi",
+				Phone:   "082291832488",
+			},
+			expectedSvc: &response.SupplierResponse{
+				ID:        1,
+				Code:      "WDWDARFSYH",
+				Name:      "Widdy Arfiansyah",
+				Address:   "Sukabumi",
+				Phone:     "082291832488",
+				CreatedAt: "0001-01-01 00:00:00 +0000 UTC",
+				UpdatedAt: "0001-01-01 00:00:00 +0000 UTC",
+			},
+			expectedSupplierRepoFindByCodeError: nil,
+			expectedSvcError:                    nil,
+		},
+		{
+			name:                                "Supplier doesnt exists with given code",
+			request:                             "WDWDARFSYH",
+			expectedSupplierRepoFindByCode:      nil,
+			expectedSvc:                         nil,
+			expectedSupplierRepoFindByCodeError: errors.New(response.NotFound),
+			expectedSvcError:                    errors.New(response.NotFound),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			var repo repository.SupplierRepositoryMock
+			repo.On("FindByCodeWithAssociations", ctx, tc.request).Return(tc.expectedSupplierRepoFindByCode, tc.expectedSupplierRepoFindByCodeError)
+			svc := NewSupplierService(&repo)
+			result, err := svc.FindByCode(ctx, tc.request)
+			if tc.expectedSvcError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expectedSvcError.Error(), err.Error())
+			}
+
+			assert.Equal(t, tc.expectedSvc, result)
+		})
+	}
+}
+
+func TestSupplierService_Create(t *testing.T) {
+
+	testCases := []struct {
+		name                            string
+		request                         *request.CreateSupplierRequest
+		expectedSupplierRepoCreate      *model.Supplier
+		expectedSupplierRepoCreateError error
+		expectedSvc                     *response.SupplierResponse
+		expectedSvcError                error
+	}{
+		{
+			name: "Create supplier with required fields",
+			request: &request.CreateSupplierRequest{
+				Name:    "Widdy Arfiansyah",
+				Address: "Sukabumi",
+				Phone:   "082291832488",
+			},
+			expectedSupplierRepoCreate: &model.Supplier{
+				ID:      1,
+				Code:    "WDWDARFSYH",
+				Name:    "Widdy Arfiansyah",
+				Address: "Sukabumi",
+				Phone:   "082291832488",
+			},
+			expectedSvc: &response.SupplierResponse{
+				ID:        1,
+				Code:      "WDWDARFSYH",
+				Name:      "Widdy Arfiansyah",
+				Address:   "Sukabumi",
+				Phone:     "082291832488",
+				CreatedAt: "0001-01-01 00:00:00 +0000 UTC",
+				UpdatedAt: "0001-01-01 00:00:00 +0000 UTC",
+			},
+			expectedSupplierRepoCreateError: nil,
+			expectedSvcError:                nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			var repo repository.SupplierRepositoryMock
+			repo.On("Create", ctx, mock.Anything).Return(tc.expectedSupplierRepoCreate, tc.expectedSupplierRepoCreateError)
+			svc := NewSupplierService(&repo)
+			result, err := svc.Create(ctx, tc.request)
+			if tc.expectedSvcError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expectedSvcError.Error(), err.Error())
+			}
+
+			assert.Equal(t, tc.expectedSvc, result)
+		})
+	}
+}
+
+func TestSupplierService_Update(t *testing.T) {
+	testCases := []struct {
+		name                                string
+		request                             *request.UpdateSupplierRequest
+		requestSupplierRepoFindByCode       string
+		expectedSupplierRepoFindByCode      *model.Supplier
+		expectedSupplierRepoFindByCodeError error
+		expectedSupplierRepoUpdate          *model.Supplier
+		expectedSupplierRepoUpdateError     error
+		expectedSvc                         *response.SupplierResponse
+		expectedSvcError                    error
+	}{
+		{
+			name: "Update supplier with required fields",
+			request: &request.UpdateSupplierRequest{
+				Code:    "WDWDARFSYH",
+				Name:    "Arfian",
+				Address: "Cisaat",
+				Phone:   "089922238421",
+			},
+			requestSupplierRepoFindByCode: "WDWDARFSYH",
+			expectedSupplierRepoFindByCode: &model.Supplier{
+				ID:      1,
+				Code:    "WDWDARFSYH",
+				Name:    "Widdy Arfiansyah",
+				Address: "Sukabumi",
+				Phone:   "082291832488",
+			},
+			expectedSupplierRepoUpdate: &model.Supplier{
+				ID:      1,
+				Code:    "WDWDARFSYH",
+				Name:    "Arfian",
+				Address: "Cisaat",
+				Phone:   "089922238421",
+			},
+			expectedSvc: &response.SupplierResponse{
+				ID:        1,
+				Code:      "WDWDARFSYH",
+				Name:      "Arfian",
+				Address:   "Cisaat",
+				Phone:     "089922238421",
+				CreatedAt: "0001-01-01 00:00:00 +0000 UTC",
+				UpdatedAt: "0001-01-01 00:00:00 +0000 UTC",
+			},
+			expectedSupplierRepoFindByCodeError: nil,
+			expectedSupplierRepoUpdateError:     nil,
+			expectedSvcError:                    nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			var repo repository.SupplierRepositoryMock
+			repo.On("FindByCode", ctx, tc.requestSupplierRepoFindByCode).Return(tc.expectedSupplierRepoFindByCode, tc.expectedSupplierRepoFindByCodeError)
+			repo.On("Update", ctx, mock.Anything).Return(tc.expectedSupplierRepoUpdate, tc.expectedSupplierRepoUpdateError)
+			svc := NewSupplierService(&repo)
+			result, err := svc.Update(ctx, tc.request)
+			if tc.expectedSvcError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expectedSvcError.Error(), err.Error())
+			}
+
+			assert.Equal(t, tc.expectedSvc, result)
+
+			// really changed
+			if err == nil {
+				assert.Equal(t, tc.expectedSupplierRepoFindByCode.Name, result.Name)
+			}
+		})
+	}
+}
+
+func TestSupplierService_Delete(t *testing.T) {
+	testCases := []struct {
+		name                                string
+		request                             string
+		expectedSupplierRepoFindByCode      *model.Supplier
+		expectedSupplierRepoFindByCodeError error
+		expectedSupplierRepoUpdateError     error
+		expectedSvcError                    error
+	}{
+		{
+			name:    "Update supplier with required fields",
+			request: "WDWDARFSYH",
+			expectedSupplierRepoFindByCode: &model.Supplier{
+				ID:      1,
+				Code:    "WDWDARFSYH",
+				Name:    "Widdy Arfiansyah",
+				Address: "Sukabumi",
+				Phone:   "082291832488",
+			},
+			expectedSupplierRepoUpdateError:     nil,
+			expectedSvcError:                    nil,
+			expectedSupplierRepoFindByCodeError: nil,
+		},
+		{
+			name:                                "Supplier doesnt exists with given ID when deleting data",
+			request:                             "WDWDARFSYH",
+			expectedSupplierRepoFindByCode:      nil,
+			expectedSupplierRepoUpdateError:     errors.New("getting an error"),
+			expectedSvcError:                    errors.New(response.NotFound),
+			expectedSupplierRepoFindByCodeError: errors.New(response.NotFound),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			var repo repository.SupplierRepositoryMock
+			repo.On("FindByCode", ctx, tc.request).Return(tc.expectedSupplierRepoFindByCode, tc.expectedSupplierRepoFindByCodeError)
+			repo.On("Delete", ctx, tc.request).Return(tc.expectedSupplierRepoUpdateError)
+			svc := NewSupplierService(&repo)
+			err := svc.Delete(ctx, tc.request)
+			if tc.expectedSvcError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expectedSvcError.Error(), err.Error())
+			}
+		})
+	}
+}
