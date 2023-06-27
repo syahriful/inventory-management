44b35d6872d577a479eef95d17c118be2be6cce5
Widdy Arfiansyah <widdyarfiansyah00@gmail.com>
2023-06-27 11:38:34 +0700

    chore: modify product request

--- backend/internal/http/request/product_quality_request.go
+++ backend/internal/http/request/product_quality_request.go
@@ -2,15 +2,15 @@ package request
 
 type CreateProductQualityRequest struct {
 	Quality  string  `json:"quality" validate:"required,max=100"`
-	Price    int64   `json:"price" validate:"required"`
-	Quantity float64 `json:"quantity" validate:"required"`
+	Price    int64   `json:"price" validate:"required,number"`
+	Quantity float64 `json:"quantity" validate:"omitempty,number"`
 	Type     string  `json:"type" validate:"required,max=20"`
 }
 
 type UpdateProductQualityRequest struct {
 	ID       int64
 	Quality  string  `json:"quality" validate:"required,max=100"`
-	Price    int64   `json:"price" validate:"required"`
-	Quantity float64 `json:"quantity" validate:"required"`
+	Price    int64   `json:"price" validate:"required,number"`
+	Quantity float64 `json:"quantity" validate:"omitempty,number"`
 	Type     string  `json:"type" validate:"required,max=20"`
 }
--- backend/internal/http/request/product_request.go
+++ backend/internal/http/request/product_request.go
@@ -4,7 +4,7 @@ type CreateProductRequest struct {
 	Name                string                         `json:"name" validate:"required,max=100"`
 	UnitMassAcronym     string                         `json:"unit_mass_acronym" validate:"required,max=20"`
 	UnitMassDescription string                         `json:"unit_mass_description" validate:"required,max=50"`
-	ProductQualities    []*CreateProductQualityRequest `json:"product_qualities" validate:"required"`
+	ProductQualities    []*CreateProductQualityRequest `json:"product_qualities" validate:"required,dive"`
 }
 
 type UpdateProductRequest struct {
@@ -12,5 +12,5 @@ type UpdateProductRequest struct {
 	Name                string                         `json:"name" validate:"required,max=100"`
 	UnitMassAcronym     string                         `json:"unit_mass_acronym" validate:"required,max=20"`
 	UnitMassDescription string                         `json:"unit_mass_description" validate:"required,max=50"`
-	ProductQualities    []*UpdateProductQualityRequest `json:"product_qualities" validate:"required"`
+	ProductQualities    []*UpdateProductQualityRequest `json:"product_qualities" validate:"required,dive"`
 }
